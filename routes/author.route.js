00c471152b60846b0391f03f8416bd938ad5804a
trangnguyen <nguyenthitrang.ttd@gmail.com>
2025-08-17 21:01:43 +0700

    🌱 update: update auth and author for category and author

--- routes/author.route.js
+++ routes/author.route.js
@@ -2,22 +2,22 @@ const express = require('express');
 const validate = require('../middlewares/validate.middleware');
 const authorController = require('../controllers/author.controller');
 const authorValidation = require('../validations/author.validation');
-// const { auth, author } = require('../middlewares/auth.middleware');
-// const { USER_ROLE_ENUM } = require('../constants');
+const { author, auth } = require('../middlewares/auth.middleware');
+const { USER_ROLE_ENUM } = require('../constants');
 
 const authorRoute = express.Router();
 
 authorRoute
   .route('/')
   .post(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(authorValidation.createAuthor),
     authorController.createAuthor,
   )
   .get(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(authorValidation.getAuthors),
     authorController.getAuthors,
   );
@@ -25,20 +25,20 @@ authorRoute
 authorRoute
   .route('/:authorId')
   .get(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(authorValidation.getAuthorById),
     authorController.getAuthorById,
   )
   .put(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(authorValidation.updateAuthorById),
     authorController.updateAuthorById,
   )
   .delete(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(authorValidation.deleteAuthorById),
     authorController.deleteAuthorById,
   );
--- routes/category.route.js
+++ routes/category.route.js
@@ -2,27 +2,29 @@ const express = require('express');
 const validate = require('../middlewares/validate.middleware');
 const categoryController = require('../controllers/category.controller');
 const categoryValidation = require('../validations/category.validation');
+const { author, auth } = require('../middlewares/auth.middleware');
+const { USER_ROLE_ENUM } = require('../constants');
 
 const categoryRoute = express.Router();
 
 categoryRoute
   .route('/')
   .post(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(categoryValidation.createCategory),
     categoryController.createCategory,
   )
   .get(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(categoryValidation.getCategories),
     categoryController.getCategories,
   );
 
 categoryRoute.route('/bulk').post(
-  // auth,
-  // author([USER_ROLE_ENUM.ADMIN]),
+  auth,
+  author([USER_ROLE_ENUM.ADMIN]),
   validate(categoryValidation.createCategories),
   categoryController.createCategories,
 );
@@ -30,20 +32,20 @@ categoryRoute.route('/bulk').post(
 categoryRoute
   .route('/:categoryId')
   .get(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(categoryValidation.getCategoryById),
     categoryController.getCategoryById,
   )
   .put(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(categoryValidation.updateCategoryById),
     categoryController.updateCategoryById,
   )
   .delete(
-    // auth,
-    // author([USER_ROLE_ENUM.ADMIN]),
+    auth,
+    author([USER_ROLE_ENUM.ADMIN]),
     validate(categoryValidation.deleteCategoryById),
     categoryController.deleteCategoryById,
   );
